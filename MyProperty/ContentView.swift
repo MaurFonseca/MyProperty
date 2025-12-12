b1262e5243b82934feb03ac1e651d722224a988c
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-12-12 09:25:10 -0300

    First commit

--- MyProperty/ContentView.swift
+++ MyProperty/ContentView.swift
@@ -1,24 +1,54 @@
 //
 //  ContentView.swift
-//  MyProperty
+//  WorkOrders
 //
-//  Created by Maurício Fonseca on 12/12/25.
+//  Created by Maurício Fonseca on 11/12/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    private let properties = ["HOUSE 123", "HOUSE 456", "HOUSE 789"]
+    @State private var selectedProperty = "HOUSE 123"
+    @State private var workOrderCount = 0
+    @State private var operatorName = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form{
+                Section{
+                    Picker("Select the property", selection: $selectedProperty){
+                        ForEach(properties, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                Section{
+                        TextField("Field Operators", text: $operatorName)
+                }
+                Section{
+                    VStack{
+                        HStack{
+                            Button("Add Work Order"){
+                                workOrderCount += 1
+                            }
+                            Spacer()
+                            Text("Number of WOs: \(workOrderCount)")
+                        }
+                        
+                    }
+                }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            Button("Reset"){
+                workOrderCount = 0
+                operatorName = ""
+            }
         }
-        .padding()
     }
 }
 
+//Para termos o preview ao lado ->
 #Preview {
     ContentView()
 }
